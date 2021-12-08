12cb91f8be70c5ac3cf5e4596259336e465feb4e
Zac Allen <zac.allen@outlook.com>
2021-12-08 12:16:57 +0100

    reverted changes

--- src/sessions/dropin.js
+++ src/sessions/dropin.js
@@ -48,8 +48,7 @@ getClientKey().then(clientKey => {
                     const dropinComponent = checkout.create('dropin', {
                         onSelect: activeComponent => {
                             if (activeComponent.state && activeComponent.state.data) updateStateContainer(activeComponent.data); // Demo purposes only
-                        },
-                        instantPaymentTypes: ['applepay']
+                        }
                     }).mount('#dropin-container');
                 }
                 initiateCheckout()
